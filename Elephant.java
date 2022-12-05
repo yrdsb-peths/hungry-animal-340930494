6df5f7a8ea3830419b9b94c51604e00baead26fb
Jacky Liang <117941640+340930494@users.noreply.github.com>
2022-12-05 14:29:13 -0500

    Add files via upload

---
+++ Elephant.java
@@ -0,0 +1,20 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Elephant here.
+ * 
+ * @author (Jacky Liang) 
+ * @version (12/05/22)
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act() 
+    {
+        //Add your code here
+        move(1);
+    }    
+}
