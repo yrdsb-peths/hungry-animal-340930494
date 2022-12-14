b78c0581dd6c212634093dd83e1c10ba6e5aeba9
Jacky Liang <340930494@gapps.yrdsb.ca>
2022-12-13 23:07:30 -0500

    3.2 Saving the world
    - Saved world from greenfoot
    - Used escape characters

--- TitleScreen.java
+++ TitleScreen.java
@@ -9,7 +9,7 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class TitleScreen extends World
 {
     Label titleLabel = new Label("Hungry Elephant", 60);
-    
+
     /**
      * Constructor for objects of class TitleScreen.
      * 
@@ -17,10 +17,11 @@ public class TitleScreen extends World
     public TitleScreen() {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-        
+
         addObject(titleLabel, getWidth()/2, getHeight()/2);
+        prepare();
     }
-    
+
     /*
      * The main world act loop
      */
@@ -30,5 +31,23 @@ public class TitleScreen extends World
             MyWorld gameWorld = new MyWorld();
             Greenfoot.setWorld(gameWorld);
         }
+    }
+    
+    /**
+     * Prepare the world for the start of the program.
+     * That is: create the initial objects and add them to the world.
+     */
+    private void prepare()
+    {
+
+        Elephant elephant = new Elephant();
+        addObject(elephant,506,60);
+        Label label = new Label("Use \u2190 and \u2192to Move", 40);
+        addObject(label,298,154);
+        label.setLocation(304,148);
+        Label label2 = new Label("Press <start> to Start", 30);
+        addObject(label2,292,350);
+        label.setLocation(279,219);
+        elephant.setLocation(514,135);
     }
 }
